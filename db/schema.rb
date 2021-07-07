a8bc13f34ff958232bea0aff9027c8229f16a6b8
a-wtnbe <ayawatanabe714@gmail.com>
2021-07-07 05:56:52 +0000

    users_fin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_06_123424) do
+ActiveRecord::Schema.define(version: 2021_07_07_024809) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_07_06_123424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

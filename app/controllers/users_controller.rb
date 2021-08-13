3a746538d8f545089b35fa04fa24de265eb0b714
a-wtnbe <ayawatanabe714@gmail.com>
2021-08-13 06:40:30 +0000

    usershow-fotter-ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
+    @article = Article.page(params[:page]).reverse_order
   end
 
   def index
